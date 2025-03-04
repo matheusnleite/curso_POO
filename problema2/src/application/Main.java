82471e79acff0b91a58d009492bc61350dff4cb9
Matheus <matheusn06leite@gmail.com>
2025-03-04 20:31:17 -0300

    refactor: enhance product stock management output and interaction, asking the quantity to be added and removed

--- problema2/src/application/Main.java
+++ problema2/src/application/Main.java
@@ -20,7 +20,20 @@ public class Main {
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
 
-        System.out.println(product);
+        System.out.println();
+        System.out.println("Product data: " + product);
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
+        System.out.println();
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.println("Updated data: " + product);
 
 
         sc.close();
