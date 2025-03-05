26f21c2bb45908cdec8095225cd19b20ca0a31cd
Matheus <matheusn06leite@gmail.com>
2025-03-05 10:27:01 -0300

    Third version of the program that calculates circumference and radius

--- problema3/src/application/Program.java
+++ problema3/src/application/Program.java
@@ -11,18 +11,17 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator();
 
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
 
-        double c = calc.circumference(radius);
+        double c = Calculator.circumference(radius);
 
-        double v = calc.volume(radius);
+        double v = Calculator.volume(radius);
 
         System.out.println("Circumference: " + String.format("%.2f", c));
         System.out.println("Volume: " + String.format("%.2f", v));
-        System.out.println("PI value: " + String.format("%.2f", calc.PI));
+        System.out.println("PI value: " + String.format("%.2f", Calculator.PI));
 
 
         sc.close();
