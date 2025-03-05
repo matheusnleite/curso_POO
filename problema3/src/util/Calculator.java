7f90a99feb2bcf50df6b48cf60c791e74e0e6f1d
Matheus <matheusn06leite@gmail.com>
2025-03-05 10:23:16 -0300

    refactor: change PI to instance variable in Calculator class

--- problema3/src/util/Calculator.java
+++ problema3/src/util/Calculator.java
@@ -1,7 +1,7 @@
 package util;
 
 public class Calculator {
-    public static final double PI = 3.14159; //declaração de constante
+    public final double PI = 3.14159; //declaração de constante
 
     public double circumference(double radius) {
         return 2.0 * PI * radius;
