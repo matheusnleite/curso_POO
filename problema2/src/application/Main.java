32bd86b9aaf6ce117e51ea48fb33324bc9dbbf67
Matheus <matheusn06leite@gmail.com>
2025-03-06 11:55:12 -0300

    refactor: enhance Product class with encapsulation and add getter/setter methods

--- problema2/src/application/Main.java
+++ problema2/src/application/Main.java
@@ -19,6 +19,12 @@ public class Main {
         double price = sc.nextDouble();
         Product product = new Product(name, price);
 
+        product.setName("Computer");
+
+        System.out.println("Updated name: " + product.getName());
+        product.setPrice(1200.00);
+        System.out.println("Updated price: " + product.getPrice());
+
         System.out.println();
         System.out.println("Product data: " + product);
         System.out.println();
--- problema2/src/entities/Product.java
+++ problema2/src/entities/Product.java
@@ -1,9 +1,12 @@
 package entities;
 
 public class Product {
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product() {
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -14,6 +17,26 @@ public class Product {
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQuantity(){
+        return quantity;
     }
 
     public double totalValueInStock() {
